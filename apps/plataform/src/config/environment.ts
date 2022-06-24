03f9ec47d29c2b407ae13dedf7545389f0c0be98
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-06-24 20:00:51 -0300

    fix: bcrypt salt should be a number

--- apps/plataform/src/config/environment.ts
+++ apps/plataform/src/config/environment.ts
@@ -7,5 +7,5 @@ dotenv.config({ path: pathName })
 
 export const DATABASE_URL:string = process.env.DATABASE_URL ?? ''
 export const MOVIE_API_KEY:string= process.env.MOVIE_API_KEY ?? ''
-export const BCRYPT_SALT:string= process.env.BCRYPT_SALT ?? '10'
+export const BCRYPT_SALT:number= Number(process.env.BCRYPT_SALT) ?? 10
 
