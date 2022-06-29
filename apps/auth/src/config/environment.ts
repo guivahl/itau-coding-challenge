a53071eb128101e5044bfee04dc1e02575ee432c
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-06-29 00:36:46 -0300

    fix: refactor environment variables types on config file

--- apps/auth/src/config/environment.ts
+++ apps/auth/src/config/environment.ts
@@ -5,9 +5,9 @@ const pathName = path.join(__dirname, '../', '../', '.env')
 
 dotenv.config({ path: pathName })
 
-export const DATABASE_URL:string = process.env.DATABASE_URL ?? ''
-export const BCRYPT_SALT:number= Number(process.env.BCRYPT_SALT) ?? 10
-export const PORT:number= Number(process.env.PORT) ?? 3001
+export const DATABASE_URL:string = process.env.DATABASE_URL ?? 'postgresql://docker:password@database:5432/itau_coding_challenge'
+export const BCRYPT_SALT:number= process.env.BCRYPT_SALT ? Number(process.env.BCRYPT_SALT) : 10
+export const PORT:number= process.env.PORT ? Number(process.env.PORT) : 3001
 export const JWT_TOKEN:string= (process.env.JWT_TOKEN) ?? 'tokenHash'
 export const JWT_EXPIRES_IN:string= (process.env.JWT_EXPIRES_IN) ?? '1h'
 
--- apps/plataform/src/config/environment.ts
+++ apps/plataform/src/config/environment.ts
@@ -5,9 +5,9 @@ const pathName = path.join(__dirname, '../', '../', '.env')
 
 dotenv.config({ path: pathName })
 
-export const DATABASE_URL:string = process.env.DATABASE_URL ?? ''
+export const DATABASE_URL:string = process.env.DATABASE_URL ?? 'postgresql://docker:password@localhost:5432/itau_coding_challenge'
 export const MOVIE_API_KEY:string= process.env.MOVIE_API_KEY ?? ''
-export const BCRYPT_SALT:number= Number(process.env.BCRYPT_SALT) ?? 10
-export const PORT:number= Number(process.env.PORT) ?? 3000
-export const URL_AUTH_API:string= process.env.URL_AUTH_API ?? 'http://localhost:3001'
+export const BCRYPT_SALT:number= process.env.BCRYPT_SALT ? Number(process.env.BCRYPT_SALT) : 10
+export const PORT:number= process.env.PORT ? Number(process.env.PORT) : 3000
+export const URL_AUTH_API:string= process.env.URL_AUTH_API ?? 'http://auth:3001'
 
